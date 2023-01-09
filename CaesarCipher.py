311c7e49573dac34446a7ea044c8db3e49e89597
lucasemmanuelsa <lucasemmanuelsa@gmail.com>
2023-01-09 19:45:58 -0300

    CaesarCipher

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -2,10 +2,6 @@ def encripty(key, text):
 
     alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    
-
-    #i = 0
-    #while i < 26:
     output = ''
     for character in text:
         if(character.isalpha() == False):
@@ -16,7 +12,7 @@ def encripty(key, text):
             else:
                 output += alpha[((alpha.index(character.upper()) + key) % 26)].lower()
             
-    #i += 1
+    
     return output
 
 print(encripty(23,input("Text: ")))
